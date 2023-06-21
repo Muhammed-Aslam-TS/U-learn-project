e57c770e55e3a500b392ede1843a5c6b15bea051
Muhammed aslam <your.email>
2023-06-21 12:41:20 +0530

    port3000

--- server/dist/FrameWorks/Webserver/Server.js
+++ server/dist/FrameWorks/Webserver/Server.js
@@ -1,9 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const Server = (app) => {
-    app.listen(4000, () => {
+    app.listen(3000, () => {
         // eslint-disable-next-line no-console
-        console.log("server start on port 4000");
+        console.log("server start on port 3000");
     });
 };
 exports.default = Server;
